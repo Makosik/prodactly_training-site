43ab8847cfc0ec818237da5d6232b5059bb82454
Maks <an-tar70@ya.ru>
2022-05-02 20:45:44 +0700

    added files js for slick-slider

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+   $('.slider').slick({
+      slidesToShow:3,
+      slidesToScroll:3,
+      arrows:false,
+      dots:true,
+      responsive:[{
+         breakpoint:1175,
+         settings:{
+            slidesToScroll:2,
+            slidesToShow:2
+         }
+      },
+      {
+         breakpoint:524,
+         settings:{
+            slidesToScroll:1,
+            slidesToShow:1
+         }
+      }
+      ]
+   });
+
+$('.menu__btn').on('click', function(){
+   $('.menu__list').toggleClass('menu__list--active')
+});
+
+});
